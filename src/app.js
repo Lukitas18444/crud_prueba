4c43f52a0655d467b39a8c2c42343d62a0c4afbd
Lucas Caballero <caballerolucas18@gmail.com>
2026-02-13 16:34:41 -0300

    prueba de mongo

--- src/app.js
+++ src/app.js
@@ -7,7 +7,16 @@ const bookRoutes = require('./routes/book.routes')
 
 config()
 
+
+// Usamos express para los middlewares
 const app = express()
+app.use(bodyParser.json()) // este es parseador de Bodies
+
+// Aca conectarmeros la base de datos:
+mongoose.connect(process.env.MONGO_URL, {dbName: process.env.MONGO_URL})
+const db = mongoose.connection
+
+app.use('/books', bookRoutes)
 
 
 const port = process.env.PORT || 3000
